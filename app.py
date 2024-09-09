3d5b14399ff59ca7c747f5fdbb2d3a6f28524343
Raj Undaviya <undaviyaraj2000@gmail.com>
2024-09-10 00:49:02 +0530

    changes

--- app.py
+++ app.py
@@ -424,5 +424,5 @@ def logout():
     return redirect('/')
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    FlaskUI(app=app, server="flask").run()
+    app.run(debug=True, host='0.0.0.0', port=5000)
+    # FlaskUI(app=app, server="flask").run()
